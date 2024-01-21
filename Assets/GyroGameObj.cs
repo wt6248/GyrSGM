deab90f0edbe6a6e23aa6d7e984d2d5192dc29c9
moondg <moondg07@outlook.com>
2024-01-21 19:15:13 +0900

    build platform -> iOS, add GyroTestScene & script

---
+++ Assets/GyroGameObj.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroGameObj : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Input.gyro.enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO
+    }
+}
