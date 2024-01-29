0c1f7f7eb19f9f0332f3d1116b784e0e13f286f5
hayeon7898 <hayeon22@korea.ac.kr>
2024-01-29 20:28:13 +0900

    Cursor & Gyro 연결 코드

--- Assets/Script/Character/Cursor.cs
+++ Assets/Script/Character/Cursor.cs
@@ -6,6 +6,7 @@ public class Cursor : MonoBehaviour
 {
     private static Cursor instance;
     private GameObject cursorObject;
+    private GyroGameObj gyroGameObj;
 
 
     private void Start()
@@ -18,10 +19,8 @@ public class Cursor : MonoBehaviour
        else{
             Debug.Log("커서 이미 생성됨");
        }
-    }
 
-    private void Update(){
-        // 자이로 센서의 값을 기반으로 커서 이동 등을 수행
+       gyroGameObj = FindObjectOfType<GyroGameObj>();
     }
     
     //커서가 커서를 생성하는 스택 오버플로우가 발생함. 수정 바람.
@@ -35,10 +34,33 @@ public class Cursor : MonoBehaviour
             cursorObject.transform.parent = transform;
             Debug.Log("커서 생성");
         }
-        else
-        {
+    }
 
-        }
+    void Update()
+    {
+        // 자이로 값을 가져와서 커서의 위치를 업데이트
+        UpdateCursorPosition();
+        Debug.Log("커서 이동");
+        Debug.Log("Cursor Position: " + transform.position);
+    }
+
+    // 커서 위치 변경 함수
+    void UpdateCursorPosition()
+    {
+        // 자이로 값 가져옴
+        Vector3 gyroValue = gyroGameObj.GetGyroValue();
+
+        // 자이로 값에 따라 커서의 이동량을 설정
+        Vector3 cursorMovement = new Vector3(gyroValue.x, gyroValue.y, 0f) * Time.deltaTime;
+
+        // 커서의 위치를 업데이트
+        transform.Translate(cursorMovement);
+
+        // 화면을 벗어나지 않도록 커서의 위치를 제한
+        Vector3 clampedPosition = transform.position;
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, -5f, 5f);
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, -3f, 3f);
+        transform.position = clampedPosition;
     }
 
     // 커서 위치 반환 함수
