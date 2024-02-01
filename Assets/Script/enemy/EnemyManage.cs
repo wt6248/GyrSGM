c5d4456f43aecca62e209329a42554847ab02e8c
moondg <moondg07@outlook.com>
2024-02-01 18:48:12 +0900

    add EnemyManage script

---
+++ Assets/Script/enemy/EnemyManage.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManage : MonoBehaviour
+{
+    List<Vector2> enemyArrange;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // initialize initial enemies
+        // enemyArrange = 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
