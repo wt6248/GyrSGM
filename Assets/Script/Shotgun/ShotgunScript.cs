cbd1c5ca51617cecd139dee1aee681f2fb506293
moondg <moondg07@outlook.com>
2024-02-02 02:58:24 +0900

    trim code

--- Assets/Script/Shotgun/ShotgunScript.cs
+++ Assets/Script/Shotgun/ShotgunScript.cs
@@ -8,24 +8,27 @@ using UnityEngine.UI;
 public class ShotgunScript : MonoBehaviour
 {
     public FixedJoystick fixedJoystick;
+    public Button FireButton;
+
     public GameObject shotgun_shell;
     public GameObject Bullet;
-    public Button FireButton;
+    Vector3 shell_drop_position = new Vector3(4.81f, 0.88f, 0f);
     
     private AudioSource audioSource;
     public AudioClip gunshotSound;
 
-    Vector3 shell_drop_position = new Vector3(4.81f, 0.88f, 0f);
 
     /*
         산탄총 발사 관련 변수
     */    
     // 총구 위치
-    public Transform firePoint; 
+    public Vector3 firePoint = new(8f, 0.88f, 0f);
     // 적 게임오브젝트
     public GameObject enemyObject;
     // 산탄총 발사 각도
-    public float shotgunAngle = 7.5f; 
+    public float shotgunAngle = 0;
+    // Spreading angle
+    public float spreadAngle = 15f;
     // 총알 개수
     public int pelletsCount = 8; 
     // 산탄 투사체 속도 계수
@@ -94,7 +97,7 @@ public class ShotgunScript : MonoBehaviour
     void CustomButton_onClick()
     {
         //Debug.Log("testtttt");
-        ShootShotgun_manually();
+        ShootShotgun();
         generate_shotgun_shell();
         makeFireSound();
     }
@@ -130,73 +133,31 @@ public class ShotgunScript : MonoBehaviour
     }
 
     // 산탄총 발사 함수
-    void ShootShotgun(GameObject nearestEnemy)
+    void ShootShotgun()
     {
-        Vector2 directionToEnemy = (nearestEnemy.transform.position - transform.position).normalized;
         for (int i = 0; i < pelletsCount; i++)
         {
-            float randomAngle = Random.Range(-shotgunAngle, shotgunAngle);
-            Quaternion randomRotation = Quaternion.AngleAxis(randomAngle, Vector3.forward);
-            Vector2 shootDirection = randomRotation * directionToEnemy;
+            float randomAngle = Random.Range(-spreadAngle, spreadAngle);
+            Quaternion error = Quaternion.AngleAxis(randomAngle, Vector3.forward);
+            Vector2 shootDirection = error * transform.rotation * Vector3.right;
 
-            //GameObject pellet = Instantiate(shotgun_shell, transform.position, Quaternion.identity);
-            GameObject pellet = Instantiate(Bullet, transform.position, Quaternion.identity);
-            Rigidbody2D rb = pellet.GetComponent<Rigidbody2D>();
+            GameObject pellet = Instantiate(Bullet, transform.position + firePoint, Quaternion.identity);
+            Debug.Log(pellet.transform.position);
+            // Rigidbody2D rb = pellet.GetComponent<Rigidbody2D>();
 
-            if (rb != null)
-            {
-                rb.velocity = shootDirection * pelletSpeedMultiplier;
-            }
-        }
-    }
-
-    void ShootShotgun_manually()
-    {
-        // Vector3 temp_firept = new Vector3( 1.0f, 1.0f , 0);
-        // Vector2 directionToEnemy = new Vector2(transform.rotation.eulerAngles.x , transform.rotation.eulerAngles.y);
-        Vector2 directionToEnemy = fixedJoystick.Direction;
-        for (int i = 0; i < pelletsCount; i++)
-        {
-            // Debug.Log(i);
-            float randomAngle = Random.Range(-shotgunAngle, shotgunAngle);
-            Quaternion randomRotation = Quaternion.AngleAxis(randomAngle, Vector3.forward);
-            Vector2 shootDirection = randomRotation * directionToEnemy;
-
-            //GameObject pellet = Instantiate(Bullet, temp_firep, Quaternion.identity);
-            //GameObject pellet = Instantiate(shotgun_shell, transform.position, Quaternion.identity);
-            GameObject pellet = Instantiate(Bullet, transform.position, Quaternion.identity);
-            Rigidbody2D rb = pellet.GetComponent<Rigidbody2D>();
-
-            if (rb != null)
-            {
-                rb.velocity = shootDirection * pelletSpeedMultiplier;
-            }
-        }
-    }
-
-    // 적을 찾아 산탄총을 발사하는 함수
-    void FindEnemiesAndShoot()
-    {
-        // 적을 찾기
-        GameObject nearestEnemy = FindNearestEnemy();
-
-        // 산탄총 발사
-        // 적이 없으면 발사하지 않음
-        if (nearestEnemy == null)
-        {
-            Debug.Log("적이 없음");
-        }
-        // 가장 가까운 적을 찾아 발사
-        else{
-            ShootShotgun(nearestEnemy);
-            Debug.Log("적을 제거함");
+            // if (rb != null)
+            // {
+            //     rb.velocity = shootDirection * pelletSpeedMultiplier;
+            // }
+            // TODO : fix this!!!!!
+            pellet.GetComponent<BulletScript>().set_velocity(0.4f, shootDirection.normalized);
         }
     }
 
     // 자동 사격 함수
     void AutoShoot()
     {        
-        FindEnemiesAndShoot();
+        ShootShotgun();
         generate_shotgun_shell();
         Debug.Log("자동 사격");
     }   
