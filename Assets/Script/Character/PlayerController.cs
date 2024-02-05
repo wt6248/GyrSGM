33dfc64e0114593d91e52b3902a40bdd0708a8e5
moondg <moondg07@outlook.com>
2024-02-06 01:28:42 +0900

    Refactor : format PlayerController and inherit Entity

--- Assets/Script/Character/PlayerController.cs
+++ Assets/Script/Character/PlayerController.cs
@@ -4,29 +4,26 @@ using UnityEngine;
 using System;
 using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : Entity
 {
-    private Cursor cursor;
-    private ShotgunController shotGun;
-    private GameObject cursorObject;
-    private SpriteRenderer spriteRenderer;
-    public GyroGameObj GyroControl;
-    
-    private static float max_hp = 5f;
-    private float hp = max_hp;
+    private Cursor _cursor;
+    private ShotgunController _shotGun;
+    private GameObject _cursorObject;
+    private SpriteRenderer _spriteRenderer;
+    public GyroGameObj _gyroControl;
 
-    private bool isInvincible = false;
+    private bool _isInvincible = false;
 
     // heal variable
-    private float heal_per_sec = 0.05f;
-    private float heal_period = 1f;
-    
+    private float _healPerSec = 0.05f;
+    private float _healPeriod = 1f;
+
 
     // audio instances
-    public AudioSource hurt_sound_source;
-    public AudioClip hurt_sound;
-    public AudioSource dead_sound_source;
-    public AudioClip dead_sound;
+    public AudioSource _hurtSoundSource;
+    public AudioClip _hurtSound;
+    public AudioSource _deadSoundSource;
+    public AudioClip _deadSound;
 
     // player size
     public const float playerRadious = 0.5f; // if chose circle collider
@@ -34,18 +31,29 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        //cursor = GetComponentInChildren<Cursor>();
+        // init stat
+        _type = Entity.EntityType.Player;
+        _maxHP = 5;
+        _hp = _maxHP;
+        _name = "Player";
+        _attackDamage = 1;
+        _speed = 4;
+        _radious = 0.5f; // if chose circle collider
+        _size = new(0.5f, 0.5f, 0f); // if chose box collider
+
+
+        //_cursor = GetComponentInChildren<Cursor>();
         CreateCursor();
-        cursor = cursorObject.GetComponent<Cursor>();
-        shotGun = GetComponentInChildren<ShotgunController>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        //GyroControl = GameObject.Find("Gyro Controller").GetComponent<GyroGameObj>();
+        _cursor = _cursorObject.GetComponent<Cursor>();
+        _shotGun = GetComponentInChildren<ShotgunController>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        //_gyroControl = GameObject.Find("Gyro Controller").GetComponent<GyroGameObj>();
 
 
-        hurt_sound_source = gameObject.AddComponent<AudioSource>();
-        hurt_sound = Resources.Load<AudioClip>("Audio/dsoof");
-        dead_sound_source = gameObject.AddComponent<AudioSource>();
-        dead_sound = Resources.Load<AudioClip>("Audio/dsplpain");
+        _hurtSoundSource = gameObject.AddComponent<AudioSource>();
+        _hurtSound = Resources.Load<AudioClip>("Audio/dsoof");
+        _deadSoundSource = gameObject.AddComponent<AudioSource>();
+        _deadSound = Resources.Load<AudioClip>("Audio/dsplpain");
 
         /*
             if player has circle collider
@@ -59,34 +67,35 @@ public class PlayerController : MonoBehaviour
         // collider.size = playerSize;
 
         // basic self healing
-        InvokeRepeating("heal", 0f, heal_period);
+        InvokeRepeating("Regenerate", 0f, _healPeriod);
     }
 
-    
+
     void Update()
     {
         //주인공(커서) 움직이기
         //MovePlayerWithCursor();
         MovePlayerWithCursor_modified();
-        
+
         //임시로 주인공 자이로 직접 움직이기
         //tempMovePlayerByGyro();
 
         //총 발사
-        if(Input.GetKeyDown(KeyCode.Space)){ //스페이스바를 누르면 발사
+        if (Input.GetKeyDown(KeyCode.Space))
+        { //스페이스바를 누르면 발사
             FireGun();
-        }   
-        
+        }
+
     }
 
     private void CreateCursor()
     {
         Debug.Log("CreateCursor() 메서드 호출됨");
-        if(cursorObject == null)
+        if (_cursorObject == null)
         {
             GameObject cursorPrefab = Resources.Load<GameObject>("Prefabs/CursorPrefab");
-            cursorObject = Instantiate(cursorPrefab, Vector3.zero, Quaternion.identity);
-            cursorObject.transform.parent = transform;
+            _cursorObject = Instantiate(cursorPrefab, Vector3.zero, Quaternion.identity);
+            _cursorObject.transform.parent = transform;
             Debug.Log("커서 생성");
         }
     }
@@ -96,7 +105,7 @@ public class PlayerController : MonoBehaviour
     void MovePlayerWithCursor()
     {
         // 커서와 플레이어 간의 상대적인 위치 계산
-        Vector3 relativePosition = cursor.CursorPosition() - transform.position;
+        Vector3 relativePosition = _cursor.CursorPosition() - transform.position;
 
         // 플레이어와 커서 간의 거리 계산
         float distance = relativePosition.magnitude;
@@ -113,120 +122,107 @@ public class PlayerController : MonoBehaviour
 
     void MovePlayerWithCursor_modified()
     {
-        if(cursor == null)
+        if (_cursor == null)
         {
-            cursor = transform.GetChild(1).gameObject.GetComponent<Cursor>();
+            _cursor = transform.GetChild(1).gameObject.GetComponent<Cursor>();
         }
-        Vector3 relativePosition = cursor.CursorLocalPosition();
-        transform.Translate(relativePosition * Time.deltaTime * 4); //4는 임의로 정한 속도. 너무 느려서 배속 넣음
-
+        Vector3 relativePosition = _cursor.CursorLocalPosition();
+        transform.Translate(relativePosition * Time.deltaTime * _speed);
     }
 
     void tempMovePlayerByGyro()
     {
-        Vector3 a = GyroControl.GetGyroValue();
+        Vector3 a = _gyroControl.GetGyroValue();
         //Debug.Log(a);
-        Vector3 b = new Vector3(a.x, a.y,0f) * Time.deltaTime * 20;
+        Vector3 b = new Vector3(a.x, a.y, 0f) * Time.deltaTime * 20;
         //Debug.Log(b);
-        transform.Translate(b); 
+        transform.Translate(b);
     }
 
     void FireGun()
     {
-        shotGun.FireGun(transform.position);
-    }   
-    private void Unbeatable() {
-        isInvincible = !isInvincible;
-
-        if (isInvincible) {
-            spriteRenderer.color = new Color(1, 1, 1, 0.5f);
-        } else {
-            spriteRenderer.color = new Color(1, 1, 1, 1);
-        }
-    } 
-    void getDamaged(float damage=1.0f, float invincible_time=0.5f) 
+        _shotGun.FireGun(transform.position);
+    }
+    private void Unbeatable()
     {
-        if (isInvincible) {
-            //Debug.Log("Invincible!");
+        _isInvincible = !_isInvincible;
+
+        if (_isInvincible)
+        {
+            _spriteRenderer.color = new Color(1, 1, 1, 0.5f);
+        }
+        else
+        {
+            _spriteRenderer.color = new Color(1, 1, 1, 1);
+        }
+    }
+    void GetDamaged(float damage = 1.0f, float invincibleTime = 0.5f)
+    {
+        if (_isInvincible)
+        {
             return;
         }
 
-        damage = 0;
-        hp -= damage;
-        if(hurt_sound != null && hp > 0f) {
-            hurt_sound_source.PlayOneShot(hurt_sound);
+        _DecreaseHP(damage);
+        if (_hurtSound != null && !IsDead())
+        {
+            _hurtSoundSource.PlayOneShot(_hurtSound);
         }
-        Debug.Log("Player HP is " + hp);
-        if (hp <= 0.0f) {
-
-            if(dead_sound != null) {
-                dead_sound_source.PlayOneShot(dead_sound);
+        Debug.Log("Player HP is " + _hp);
+        if (IsDead())
+        {
+            if (_deadSound != null)
+            {
+                _deadSoundSource.PlayOneShot(_deadSound);
             }
             Debug.Log("Player Died");
-            CancelInvoke("heal");
+            CancelInvoke("Regenerate");
 
             /*
                 Enemy finds player object -> do not destroy player
             */
             CancelInvoke("AutoShoot");
             Time.timeScale = 0;
-            Button FireButton =  GameObject.Find("Fire Button").GetComponent<Button>();
+            Button FireButton = GameObject.Find("Fire Button").GetComponent<Button>();
             FireButton.onClick.RemoveAllListeners();
             // Destroy(this.gameObject);
             return;
         }
 
-        // 무적 
+        // 무적
         Unbeatable();
-        Invoke("Unbeatable", invincible_time);
-
-
+        Invoke("Unbeatable", invincibleTime);
     }
-    void OnCollisionEnter2D(Collision2D other) {
+    void OnCollisionEnter2D(Collision2D other)
+    {
         bool local_debug = false;
-        int n_attack = local_debug ? 2:1;
+        int n_attack = local_debug ? 2 : 1;
 
-
-
-
-
-
-        
         StartCoroutine(DamageRoutine(1f, 0.1f));
-            
-        
+
 
         // test code for the function operates well
-        if (hp > 0 && local_debug) {
+        if (_hp > 0 && local_debug)
+        {
             other.gameObject.transform.position = new Vector3(10, 10, 0);
         }
-        
-        
-
     }
 
 
     IEnumerator DamageRoutine(float damage, float period)
     {
-        while (hp > 0)
+        while (_hp > 0)
         {
             // give damage
-            getDamaged(damage);
+            GetDamaged(damage);
 
             // wait
             yield return new WaitForSeconds(period);
         }
     }
 
-    void heal()
+    void Regenerate()
     {
-        hp += heal_per_sec;
-        if (hp >= max_hp) {
-            hp = max_hp;
-            //Debug.Log("Full HP: "+ hp);
-            return;
-        } else {
-            Debug.Log("Healed: " + hp);
-        }
+        _IncreaseHP(_healPerSec);
     }
 }
