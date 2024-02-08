aecaf64bd98072f66d2a115f6ae496a394dfadd7
hayeon7898 <hayeon22@korea.ac.kr>
2024-02-08 20:48:01 +0900

    Feat: all item's functionality function

--- Assets/ItemPrefabScript.cs
+++ Assets/ItemPrefabScript.cs
@@ -33,9 +33,17 @@ public class ItemPrefabScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other) {
         //주인공과 충돌했을 때 주인공의 함수 호출. 
-        if(other.gameObject.name == "main character")
+        if(other.gameObject.name == "main character" && playerController != null)
         {
-            if(itemType == 4 && playerController != null)
+            if (itemType == 0 || itemType == 1)
+            {
+                playerController.SetAttackDamage();
+            }
+            else if(itemType == 2 || itemType == 3)
+            {
+                playerController.SetSpeed();
+            }
+            else // item 4, 5 (체력키트, 알약)
             {
                 playerController.SetHealthPoint();
             }
