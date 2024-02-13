deb7baf04ec9f8565add9f41028209ac8e2c1de0
moondg <moondg07@outlook.com>
2024-02-13 19:05:35 +0900

    Fix : publish branch and remove Destroy

--- Assets/Script/Bullet/BulletScript.cs
+++ Assets/Script/Bullet/BulletScript.cs
@@ -9,8 +9,7 @@ public class BulletScript : Bullet
     // Start is called before the first frame update
     void Start()
     {
-        
-        Destroy(gameObject, 3);
+
     }
 
     // Update is called once per frame
