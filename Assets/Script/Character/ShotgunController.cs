43abcea27437281e8dc21d9d515e830eed18182e
hayeon7898 <hayeon22@korea.ac.kr>
2024-02-26 20:22:07 +0900

    Fix: shotgun invisible at the beginning

--- Assets/Script/Character/ShotgunController.cs
+++ Assets/Script/Character/ShotgunController.cs
@@ -50,6 +50,7 @@ public class ShotgunController : MonoBehaviour
         GameObject shotgunPrefab = Resources.Load<GameObject>("Prefabs/ShotgunPrefab");
         GameObject shotgunObject = Instantiate(shotgunPrefab, Vector3.zero, Quaternion.identity);
         shotgunObject.transform.parent = transform;
+        shotgunObject.transform.rotation = Quaternion.Euler(0f, 0f, 90f);
         playerController = FindObjectOfType<PlayerController>();
 
         // bullet
