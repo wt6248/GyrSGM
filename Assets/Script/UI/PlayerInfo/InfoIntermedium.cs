e77c81776c09a82690230f19d89fbfde8226d56f
wt6248 <wt6248@gmail.com>
2024-02-27 20:38:28 +0900

    Feat : add Info script, move script to script UI folder

--- Assets/Script/UI/PlayerInfo/InfoIntermedium.cs
+++ Assets/Script/UI/PlayerInfo/InfoIntermedium.cs
@@ -6,21 +6,29 @@ public class InfoIntermedium : MonoBehaviour
 {
     public PlayerController playerScript;
     public GaugeManager gaugeScript;
+    public TextDisplayScript attackspeedScript;
+    public TextDisplayScript damageMultiplierScript;
     public float playermaxhp;
     // Start is called before the first frame update
     void Start()
     {
         if(playerScript == null)
             playerScript = GameObject.Find("Main Character").GetComponent<PlayerController>();
+
         if(gaugeScript == null)
             gaugeScript = gameObject.GetComponent<GaugeManager>();
-
         gaugeScript.SetMaxValue(playerScript._maxHP);
+
+        attackspeedScript._displayType = TextDisplayScript.displaytype.AttackSpeed;
+        damageMultiplierScript._displayType = TextDisplayScript.displaytype.DamageMultiplier;
+
     }
 
     // Update is called once per frame
     void Update()
     {
         gaugeScript.SetCurrentValue(playerScript.HealthPointManager());
+        attackspeedScript.setValue(playerScript.AttackSpeedManager());
+        damageMultiplierScript.setValue(playerScript.AttackDamageManager());
     }
 }
---
+++ Assets/Script/UI/PlayerInfo/TextDisplayScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextDisplayScript : MonoBehaviour
+{
+    public TextMeshProUGUI thisTMPro;
+    public float _value = 1f;
+
+    public enum displaytype
+    {
+        AttackSpeed,
+        DamageMultiplier
+    }
+    public displaytype _displayType;
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisTMPro = GetComponent<TextMeshProUGUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        displayByType(_displayType);
+    }
+
+    public void displayByType(displaytype _type)
+    {
+        switch(_type)
+        {
+            case displaytype.AttackSpeed:
+                thisTMPro.text = "Speed: " + _value.ToString("F2");
+                break;
+            case displaytype.DamageMultiplier:
+                thisTMPro.text =  "Damage: " + _value.ToString("F1");
+                break;
+            default:
+                thisTMPro.text =  "Something: " + _value.ToString();
+                break;
+        }
+    }
+    public void setValue(float _inputValue)
+    {
+        _value = _inputValue;
+    }
+}
