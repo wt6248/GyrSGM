0f3731275f8d47cb7c83224f6ab8d8540e0909b1
hayeon7898 <hayeon22@korea.ac.kr>
2024-02-28 18:44:12 +0900

    Fix: changing gun button -> made guns rotate

--- Assets/Script/ChangeGunButton.cs
+++ Assets/Script/ChangeGunButton.cs
@@ -5,15 +5,25 @@ using UnityEngine.UI;
 
 public class ChangeGun : MonoBehaviour
 {
-    public GameObject gunTypePrefab; // Canvas의 Transform 컴포넌트
-    public GameObject obj;
-    public Image[] gunImages; // GunType1, GunType2, GunType3 이미지들을 저장할 배열
-    public int currentImageIndex = 0; // 현재 이미지의 인덱스를 나타내는 변수
+    public GameObject gunTypePrefab; 
+    public GameObject gunType1;
+    public GameObject gunType2;
+    public GameObject gunType3;
+    // GunType1, GunType2, GunType3 이미지들을 저장할 배열
+    public Image[] gunImages; 
+    // 이미지 배열의 인덱스
+    public int currentImageIndex1 = 0; 
+    public int currentImageIndex2 = 1; 
+    public int currentImageIndex3 = 2; 
 
     void Start()
     {
         gunTypePrefab = Resources.Load("Prefabs/GunType") as GameObject;
-        // GunType1, GunType2, GunType3 이미지들을 가져오기
+        gunType1 = GameObject.Find("GunType_1");
+        gunType2 = GameObject.Find("GunType_2");
+        gunType3 = GameObject.Find("GunType_3");
+       
+        // GunType1, GunType2, GunType3 이미지들 가져오기
         gunImages = new Image[3];
         gunImages[0] = gunTypePrefab.transform.Find("Canvas/GunType1").GetComponent<Image>();
         gunImages[1] = gunTypePrefab.transform.Find("Canvas/GunType2").GetComponent<Image>();
@@ -23,18 +33,25 @@ public class ChangeGun : MonoBehaviour
         UpdateImage();
     }
 
-    // 버튼을 클릭할 때 호출될 함수
     public void ChangeImage()
     {
         // 다음 이미지 인덱스로 변경
-        currentImageIndex = (currentImageIndex + 1) % 3;
+        currentImageIndex1 = (currentImageIndex1 + 1) % 3;
+        currentImageIndex2 = (currentImageIndex2 + 1) % 3;
+        currentImageIndex3 = (currentImageIndex3 + 1) % 3;
+
         UpdateImage();
     }
 
-    // 현재 이미지 업데이트 함수
     void UpdateImage()
     {
-        Sprite updateSprite = gunImages[currentImageIndex].sprite;
-        gunTypePrefab.GetComponent<Image>().sprite = updateSprite;
+        //3개의 총 이미지 오브젝트 스프라이트 변경하기
+        Image imageComponent1 = gunType1.GetComponent<Image>();
+        Image imageComponent2 = gunType2.GetComponent<Image>();
+        Image imageComponent3 = gunType3.GetComponent<Image>();
+
+        imageComponent1.sprite = gunImages[currentImageIndex1].sprite;
+        imageComponent2.sprite = gunImages[currentImageIndex2].sprite;
+        imageComponent3.sprite = gunImages[currentImageIndex3].sprite;
     }
 }
